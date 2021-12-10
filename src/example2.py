Added adjoint visualization mode and corrected the jacobian of the vortex wind. Also minor improvements.

src/example2.py
@@ -20,7 +20,7 @@ def example2():
     # UAV goal point x-coordinate in meters
     x_f = 1.
     # The time window upper bound in seconds
-    T = 1.5
+    T = 1.
 
     vortex1 = VortexWind(0.5, 0.7, -1.)
     vortex2 = VortexWind(0.8, 0.2, -0.5)
@@ -29,7 +29,7 @@ def example2():
 
     # Wind allows linear composition
     total_wind = 3. * const_wind + vortex1 + vortex2 + vortex3
-    vortex1.value(np.array([0.3, 0.5]))
+    vortex1.value(np.array([0., 0.]))
     const_wind.value(np.array([0.3, 0.5]))
     total_wind.value(np.array([0.3, 0.5]))
 
@@ -37,20 +37,23 @@ def example2():
     zermelo_model = ZermeloGeneralModel(v_a, x_f)
     zermelo_model.update_wind(total_wind)
 
+    # Initial point
+    x_init = np.array([0.6, -0.1])
+
     # Creates the navigation problem on top of the previous model
     mp = MermozProblem(zermelo_model, T=T)
     mp.display.set_wind_density(2)
 
     # Set a list of initial adjoint states for the shooting method
-    list_p = list(map(lambda theta: np.array([np.cos(theta), np.sin(theta)]),
-                      np.linspace(3 * np.pi / 4. + 1e-3, 5 * np.pi / 4. - 1e-3, 100)))
+    initial_headings = np.linspace(- np.pi + 1e-3, np.pi - 1e-3, 1000)
+    list_p = list(map(lambda theta: -np.array([np.cos(theta), np.sin(theta)]), initial_headings))
 
     # Get time-optimal candidate trajectories as integrals of
     # the augmented system using the shooting method
     # The control law is a result of the integration and is
     # thus implicitly defined
     for p in list_p:
-        shoot = Shooting(zermelo_model.dyn, np.zeros(2), T, N_iter=100)
+        shoot = Shooting(zermelo_model.dyn, x_init, T, N_iter=100)
         shoot.set_adjoint(p)
         aug_traj = shoot.integrate()
         mp.trajs.append(aug_traj)
@@ -62,9 +65,9 @@ def example2():
     list_headings = np.linspace(-0.6, 1.2, 20)
     for heading in list_headings:
         mp.load_feedback(FixedHeadingFB(mp._model.wind, v_a, heading))
-        mp.integrate_trajectory(TimedSC(T), int_step=0.01)
+        mp.integrate_trajectory(x_init, TimedSC(T), int_step=0.01)
 
-    mp.plot_trajs(mode="reachability")
+    mp.plot_trajs(color_mode="reachability")
 
 
 if __name__ == '__main__':

src/visual.py
@@ -10,15 +10,30 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 from src.trajectory import Trajectory, AugmentedTraj
 
+my_red = np.array([0.8, 0., 0., 1.])
+my_red_t = np.diag((1., 1., 1., 0.2)).dot(my_red)
+my_orange = np.array([0.8, 0.3, 0., 1.])
+my_orange_t = np.diag((1., 1., 1., 0.5)).dot(my_orange)
+my_blue = np.array([0., 0., 0.8, 1.])
+my_blue_t = np.diag((1., 1., 1., 0.5)).dot(my_blue)
+my_black = np.array([0., 0., 0., 1.])
+
 reachability_colors = {
     "pmp": {
         "steps": np.array([0.8, 0.8, 0.8, 0.6]),
-        "last": np.array([0.8, 0., 0., 1.])
+        "last": my_red
     },
     "integral": {
         "steps": np.array([0.75, 0.75, 0.75, 0.6]),
-        "last": np.array([0., 0., 0.8, 1.])
+        "last": my_blue
     }
+}
+
+monocolor_colors = {
+    "pmp": my_red_t,
+    "approx": my_orange_t,
+    "point": my_blue,
+    "integral": my_black
 }
 
 state_names = [r"$x\:[m]$", r"$y\:[m]$"]
@@ -77,7 +92,7 @@ class Visual:
         self.ny_wind = ny_wind
 
         self.x_min, self.x_max = -.1, 1.1
-        self.y_min, self.y_max = -1., 1.
+        self.y_min, self.y_max = -.75, .75
 
         self.fig = None
         self.map = None
@@ -164,15 +179,12 @@ class Visual:
 
         self.map.grid(visible=True, linestyle='-.', linewidth=0.5)
         self.map.tick_params(direction='in')
+        self.map.axis('equal')
 
     def setup_map_adj(self):
         """
-        Sets the display of the map
+        Sets the display of the map for the adjoint state
         """
-        self.map_adjoint.axhline(y=0, color='k', linewidth=0.5)
-        self.map_adjoint.axvline(x=0, color='k', linewidth=0.5)
-        self.map_adjoint.axvline(x=1., color='k', linewidth=0.5)
-
         self.map_adjoint.set_xlim(-1.1, 1.1)
         self.map_adjoint.set_ylim(-1.1, 1.1)
 
@@ -181,6 +193,7 @@ class Visual:
 
         self.map_adjoint.grid(visible=True, linestyle='-.', linewidth=0.5)
         self.map_adjoint.tick_params(direction='in')
+        self.map_adjoint.axis('equal')
 
     def set_wind_density(self, level: int):
         """
@@ -250,7 +263,7 @@ class Visual:
             if k == len(self.control) - 1:
                 control_plot.set_xlabel(r"$t\:[s]$")
 
-    def plot_traj(self, traj: Trajectory, mode="default"):
+    def plot_traj(self, traj: Trajectory, color_mode="default"):
         """
         Plots the given trajectory according to selected display mode
         """
@@ -258,10 +271,13 @@ class Visual:
             self.setup()
         label = None
         s = 0.5 * np.ones(traj.last_index)
-        if mode == "default":
+        if color_mode == "default":
             colors = None
             cmap = None
-        elif mode == "reachability":
+        elif color_mode == "monocolor":
+            colors = np.tile(monocolor_colors[traj.type], traj.last_index).reshape((traj.last_index, 4))
+            cmap = None
+        elif color_mode == "reachability":
             colors = np.ones((traj.last_index, 4))
             colors[:] = np.einsum("ij,j->ij", colors, reachability_colors[traj.type]["steps"])
 
@@ -269,7 +285,7 @@ class Visual:
             s[-1] = 2.
             cmap = plt.get_cmap("YlGn")
         else:
-            raise ValueError(f"Unknown plot mode {mode}")
+            raise ValueError(f"Unknown plot mode {color_mode}")
         s *= 3.
         self.map.scatter(traj.points[:traj.last_index, 0], traj.points[:traj.last_index, 1],
                          s=s,

src/wind.py
@@ -127,7 +127,7 @@ class VortexWind(Wind):
         x_omega = self.x_omega
         y_omega = self.y_omega
         return self.gamma / (2 * np.pi * r ** 4) * \
-               np.array([[-2 * (x[0] - x_omega) * (x[1] - y_omega), (x[1] - y_omega) ** 2 - (x[0] - x_omega) ** 2],
+               np.array([[2 * (x[0] - x_omega) * (x[1] - y_omega), (x[1] - y_omega) ** 2 - (x[0] - x_omega) ** 2],
                          [(x[1] - y_omega) ** 2 - (x[0] - x_omega) ** 2, -2 * (x[0] - x_omega) * (x[1] - y_omega)]])
 
 