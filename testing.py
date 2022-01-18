4a9620c2eadd109a9d307c029970f3c6f390b9cd
Bastien <bastien.schnitzler@live.fr>
2022-01-18 16:26:11 +0100

    Minor changes to the testing.py file

--- testing.py
+++ testing.py
@@ -1,34 +1,47 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from math import atan, tan, exp
+
 
 def run():
-    l = 3.
-    def f(x, l):
-        return -l * np.sin(2*x)
+    lambda_0 = 2.
+    l = lambda_0 / 2.
 
-    for x0 in [0.1, 1., np.pi / 2. + 1e-2, np.pi / 2. + 1e-3]:
+    def f(x, l):
+        return -l * np.sin(2 * x)
+
+    for theta_0 in [0.1, 1., np.pi / 2. + 1e-2, np.pi / 2. + 1e-3]:
         t = 0.
         t_list = [t]
         dt = 0.001
-        x = x0
-        x_list = [x]
+        theta = theta_0
+        theta_list = [theta]
         for i in range(5000):
-            x = x + f(x, l)*dt
-            x_list.append(x)
+            theta = theta + f(theta, l) * dt
+            theta_list.append(theta)
             t += dt
             t_list.append(t)
 
-        plt.plot(t_list, x_list, label=f'$x_0=${x0}')
-    # atan = []
-    # for t in t_list:
-    #     atan.append(3* np.pi /4. + 0.5 * np.arctan(3.*(t-2.33)))
-    # plt.plot(t_list, atan, label=f'arctan')
-    tanh = []
-    for t in t_list:
-        tanh.append(3* np.pi /4. + np.pi/4 * np.tanh(1.85*(t-2.3)))
-    plt.plot(t_list, tanh, label=f'tanh')
+        plt.plot(t_list, theta_list, label=f'$theta_0=${theta_0:.2f}')
+
+    theta_0 = -1.5
+
+    def model(theta_0, t, lambda_0):
+        const = 0.
+        if theta_0 < -np.pi / 2.:
+            const = -np.pi
+        if np.pi / 2. < theta_0:
+            const = np.pi
+        return const + atan(tan(theta_0) * exp(-lambda_0 * t))
+
+    for theta_0 in [-2., -1., 1., 2., 3., 4., 5.]:
+        model_list = []
+        for t in t_list:
+            model_list.append(model(theta_0, t, lambda_0))
+        plt.plot(t_list, model_list, label=f'model for $theta_0=${theta_0:.2f}')
     plt.legend()
     plt.show()
+
 
 if __name__ == '__main__':
     run()
