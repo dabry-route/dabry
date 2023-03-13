6d382d2ac68f1879e0c6907224bc64237d5b8b65
Bastien <bastien.schnitzler@live.fr>
2023-03-13 10:59:11 +0100

    Added setup script

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dabry',
+    version='1.0',
+    description='Trajectory optimization in flow fields',
+    author='Bastien Schnitzler',
+    author_email='bastien.schnitzler@live.fr',
+    packages=find_packages('src'),  # same as name
+    package_dir={'': 'src'},
+    install_requires=['basemap',
+                      'cdsapi',
+                      'ecmwf-api-client',
+                      'geopy',
+                      'h5py',
+                      'Markdown',
+                      'matplotlib',
+                      'numpy',
+                      'parse',
+                      'pygrib',
+                      'pyproj',
+                      'scipy',
+                      'Shapely',
+                      'tqdm'],
+)
